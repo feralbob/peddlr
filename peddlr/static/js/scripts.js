7299e0de5834d3df543650e3c45c03df4a3c15fd
andrew-drake <andrew@resonancedev.ca>
2013-08-24 16:15:55 -0300

    Location js working

---
+++ peddlr/static/js/scripts.js
@@ -0,0 +1,15 @@
+// Get the current location
+$(document).ready(function() {
+    navigator.geolocation.getCurrentPosition(setPosition);
+});
+
+function setPosition(position) {
+    // Set our lat and lon fields with jquery
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    $('#id_location_0').val(latitude);
+    $('#id_location_1').val(longitude);
+}
+
+
+

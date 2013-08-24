4fc182d416567f4f78c613a186b9efe3fa619b93
andrew-drake <andrew@resonancedev.ca>
2013-08-24 13:21:27 -0300

    Updated models

---
+++ peddlr/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Checkin(models.Model):
+    seller_name = models.CharField(max_length=255)
+    location = models.()
+    items = models.ManyToManyField(Items,)
+    time = models.DateTimeField(auto_now=True)
+    expiry = models.DateTimeField(help_text=u'Seconds')
+
+
+class Items(models.Model):
+    name = models.CharField(max_length=255, )
+
+
+class CheckinItem(models.Model):
+    price = models.CharField(max_length=100, )
+    unit = models.CharField(max_length=55, help_text=u'Per lb or per item')
+    item = models.ForeignKey(Items,)
+    checkin = models.ForeignKey(Checkin,)
+

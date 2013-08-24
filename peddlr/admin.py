e50f07595b709cacfd760f995789f7aed2794e57
andrew-drake <andrew@resonancedev.ca>
2013-08-24 16:26:15 -0300

    admin updates

--- peddlr/admin.py
+++ peddlr/admin.py
@@ -2,21 +2,24 @@ from django.contrib import admin
 from models import Checkin, CheckinItem, Item
 
 
-#class Inline(admin.TabularInline):
-#    model =
+class CheckinItemInline(admin.TabularInline):
+    model = CheckinItem
+    fields = ('item', 'price', 'unit', 'checkin',)
+    extra = 1
 
 
 class CheckinAdmin(admin.ModelAdmin):
     save_on_top = True
+    inlines = (CheckinItemInline, )
     fields = ('seller_name', 'location', 'expiry')
     list_display = ('seller_name', 'location', 'time', 'expiry')
 
 
 class CheckinItemAdmin(admin.ModelAdmin):
     save_on_top = True
-    fields = ('price', 'unit', 'item', 'checkin', 'options')
+    fields = ('price', 'unit', 'item', 'checkin',)
     readonly_fields = ()
-    list_display = ('price', 'unit', 'item', 'checkin', 'options')
+    list_display = ('price', 'unit', 'item', 'checkin',)
 
 
 class ItemsAdmin(admin.ModelAdmin):
